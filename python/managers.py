5fedab7d53690a77191b91dde2845f316dc9bac5
Rob Rowe <rippinrobr@gmail.com>
2016-10-20 15:36:06 -0400

    changed the way the object is initialized and cleaned up an if/else

--- python/managers.py
+++ python/managers.py
@@ -9,11 +9,22 @@ manager_files = [base_dir+"/Managers.csv",
         base_dir+"/AwardsShareManagers.csv"]
 # end of settings info
 
+managers = dict() 
+
 class RawManager:
     """ A representation of a manager's managerial stats as read from CSV file """
-    def __init__(self, playerId, season):
+    def __init__(self, playerId):
         self.playerId = playerId
-        self.seasons = [season]
+        self.seasons = []
+        self.awards = []
+
+    def add_stats(self, stat_prop, stats):
+        if stat_prop == "seasons":
+            self.seasons.append( stats )
+
+        if stat_prop == "awards":
+            self.awards.append( stats )
+
 
 def list_to_dict(headers, data):
     d = {}
@@ -22,16 +33,12 @@ def list_to_dict(headers, data):
     for i in range(0, len(headers)-1):
         if i < num_cols and i < num_data_cols:
             if headers[i]:
-                print i, "=>", headers[i]
                 d[ headers[i] ] = data[i]
-                print headers[i], " => ",  data[i]
-
     return d
 
-managers = dict() 
-is_header_row = True
-csv_headers = []
 with open(manager_files[0])  as csvfile:
+    is_header_row = True
+    csv_headers = []
 
     mgr_reader = csv.reader(csvfile, delimiter=',')
     for mgr_season in mgr_reader:
@@ -41,13 +48,13 @@ with open(manager_files[0])  as csvfile:
             continue
 
         season_dict = list_to_dict(csv_headers, mgr_season[1:])
-        if mgr_season[0] not in managers:
-            if mgr_season[0]:
-                managers[mgr_season[0]] = RawManager(mgr_season[0], season_dict)
-            else:
-                managers[mgr_season[0]].seasons.append(season_dict)
+        if mgr_season[0]:
+            if mgr_season[0] not in managers:
+                managers[mgr_season[0]] = RawManager(mgr_season[0]) #, season_dict)
+            
+            managers[mgr_season[0]].add_stats("seasons", season_dict)
 
-#print("hello")
+print( managers["bochybr01"].seasons )
 #for k in managers:
 #    print( managers[k].seasons)
 
