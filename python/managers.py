5025a8e08a1485d248a7041ba4ca85037e2a381f
Rob Rowe <rippinrobr@gmail.com>
2016-10-20 15:45:19 -0400

    Now have all three playerId in col 0 files processing correctly

--- python/managers.py
+++ python/managers.py
@@ -30,31 +30,39 @@ def list_to_dict(headers, data):
     d = {}
     num_cols = len(headers)
     num_data_cols = len(data)
-    for i in range(0, len(headers)-1):
+    for i in range(0, len(headers)):
         if i < num_cols and i < num_data_cols:
             if headers[i]:
                 d[ headers[i] ] = data[i]
     return d
 
-with open(manager_files[0])  as csvfile:
-    is_header_row = True
-    csv_headers = []
+def process_manager_files_with_id_in_first_col(file_path, stat_prop):
+    
+    with open(file_path)  as csvfile:
+        is_header_row = True
+        csv_headers = []
 
-    mgr_reader = csv.reader(csvfile, delimiter=',')
-    for mgr_season in mgr_reader:
-        if is_header_row:
-            csv_headers = mgr_season[1:]
-            is_header_row = False
-            continue
+        mgr_reader = csv.reader(csvfile, delimiter=',')
+        for mgr_season in mgr_reader:
+            if is_header_row:
+                csv_headers = mgr_season[1:]
+                is_header_row = False
+                continue
 
-        season_dict = list_to_dict(csv_headers, mgr_season[1:])
-        if mgr_season[0]:
-            if mgr_season[0] not in managers:
-                managers[mgr_season[0]] = RawManager(mgr_season[0]) #, season_dict)
+            stats_dict = list_to_dict(csv_headers, mgr_season[1:])
+            if mgr_season[0]:
+                if mgr_season[0] not in managers:
+                    managers[mgr_season[0]] = RawManager(mgr_season[0])
             
-            managers[mgr_season[0]].add_stats("seasons", season_dict)
+                managers[mgr_season[0]].add_stats(stat_prop, stats_dict)
+
+# start the processing
+process_manager_files_with_id_in_first_col(manager_files[0], "seasons")
+process_manager_files_with_id_in_first_col(manager_files[1], "seasons")
+process_manager_files_with_id_in_first_col(manager_files[2], "awards")
 
 print( managers["bochybr01"].seasons )
+print( managers["bochybr01"].awards )
 #for k in managers:
 #    print( managers[k].seasons)
 
