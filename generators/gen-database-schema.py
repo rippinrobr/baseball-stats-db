8a244db550fb1ce4f69ca21e9df7b177cafeb8c3
Rob Rowe <rrowe@foodlogiq.com>
2017-11-08 21:30:50 -0500

    added a command line arg to allow the user to write the Sqlite db where they'd like

--- generators/gen-database-schema.py
+++ generators/gen-database-schema.py
@@ -3,6 +3,7 @@ from sql_schema import *
 SQLITE = "sqlite"
 def define_parameters(parser):
     parser.add_argument("--dbtype", choices=[SQLITE], help="the database type you'd like to generate the schema for", type=str, default="sqlite" )
+    parser.add_argument("--dbpath", help="SQLITE ONLY - the path for the newly created database", type=str)
 
 def main():
     param_parser = argparse.ArgumentParser(description="Generates a DB schema based on the Baseball Databank csv files.")    
@@ -10,8 +11,15 @@ def main():
     args = param_parser.parse_args()
 
     bdb_db = ""
+    sqlite_db_name = "baseball_databank.sqlite"
+    if args.dbpath != None and args.dbpath != "":
+        if args.dbtype == SQLITE:
+            sqlite_db_name = args.dbpath 
+        else:
+            print "The argument --dbpath is only needed for Sqlite databases, ignoring."
+
     if args.dbtype == SQLITE:
-        bdb_db = SqliteDatabase('db/bdb_v0.0.1.sqlite')
+        bdb_db = SqliteDatabase(sqlite_db_name)
 
     initialize_db_and_connect(bdb_db)
     
