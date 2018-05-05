b50e57f887cca18282e7282108ef902fbb7ff35a
Rob Rowe <rrowe@foodlogiq.com>
2018-05-05 19:29:55 -0400

    added -inputfiles so I can update the db with files that changed

--- internal/databank/databank.go
+++ internal/databank/databank.go
@@ -57,3 +57,66 @@ func GetTableObjects() []TableObject {
 		&TeamsHalf{},
 	}
 }
+
+// LookupTableObject returns the TableObject that relates to
+// the file being processed
+func LookupTableObject(fileName string) TableObject {
+	switch fileName {
+	case "People.csv":
+		return &People{}
+	case "AllstarFull.csv":
+		return &AllstarFull{}
+	case "Appearances.csv":
+		return &Appearances{}
+	case "AwardsManagers.csv":
+		return &AwardsManagers{}
+	case "AwardsPlayers.csv":
+		return &AwardsPlayers{}
+	case "AwardsShareManagers.csv":
+		return &AwardsShareManagers{}
+	case "AwardsSharePlayers.csv":
+		return &AwardsSharePlayers{}
+	case "Batting.csv":
+		return &Batting{}
+	case "BattingPost.csv":
+		return &BattingPost{}
+	case "CollegePlaying.csv":
+		return &CollegePlaying{}
+	case "Fielding.csv":
+		return &Fielding{}
+	case "FieldingOF.csv":
+		return &FieldingOF{}
+	case "FieldingPost.csv":
+		return &FieldingPost{}
+	case "FieldingOFsplit.csv":
+		return &FieldingOFsplit{}
+	case "HallOfFame.csv":
+		return &HallOfFame{}
+	case "HomeGames.csv":
+		return &HomeGames{}
+	case "Managers.csv":
+		return &Managers{}
+	case "ManagersHalf.csv":
+		return &ManagersHalf{}
+	case "Parks.csv":
+		return &Parks{}
+	case "Pitching.csv":
+		return &Pitching{}
+	case "PitchingPost.csv":
+		return &PitchingPost{}
+	case "Salaries.csv":
+		return &Salaries{}
+	case "Schools.csv":
+		return &Schools{}
+	case "SeriesPost.csv":
+		return &SeriesPost{}
+	case "Teams.csv":
+		return &Teams{}
+	case "TeamsFranchises.csv":
+		return &TeamsFranchises{}
+	case "TeamsHalf.csv":
+		return &TeamsHalf{}
+	}
+
+	return nil //&People{}
+}
